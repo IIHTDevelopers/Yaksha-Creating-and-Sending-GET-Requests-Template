8b88d77fea95cadc9cd7c7f6f55a13398ce574f1
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-03-31 07:14:51 +0530

    Ameet - Update AppController.java
    Update AppController.java for template

--- src/main/java/com/yaksha/assignment/controller/AppController.java
+++ src/main/java/com/yaksha/assignment/controller/AppController.java
@@ -5,18 +5,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@RestController
 public class AppController {
-
-	private final RestTemplate restTemplate;
-
-	public AppController(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
-	}
-
-	@GetMapping("/fetchData")
-	public String fetchDataFromApi(@RequestParam String apiUrl) {
-		String response = restTemplate.getForObject(apiUrl, String.class);
-		return response;
-	}
+	// write your logic here
 }
